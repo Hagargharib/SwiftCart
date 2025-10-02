ea5b2053e520da58341eaaa558f584262f6267b3
Hagargharib <hagar.mohamed.22230774@commerce.helwan.edu.eg>
2025-10-03 00:39:13 +0300

    Fix TypeScript any errors and improve typings

--- src/app/(pages)/products/[id]/page.tsx
+++ src/app/(pages)/products/[id]/page.tsx
@@ -2,13 +2,21 @@ import { getProductsDetails } from '@/actions/products.action';
 import ProductDetailsCompo from '@/components/products-compo/ProductDetailsCompo';
 import React from 'react'
 
-export default async function ProductDetails({params}:{params:{id:string}}) {
-const {id} = await params;
-const{data: productDetails}=await getProductsDetails(id);
+export default async function ProductDetails({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const response = await getProductsDetails(id);
+
+  if (!response?.data) {
+    return <div>Product not found</div>;
+  }
+
+  const productDetails = response.data;
+
 
   return (
     <div className='container mx-auto' >
-      <ProductDetailsCompo productDetails={productDetails} />  
+      <ProductDetailsCompo productDetails={productDetails} />
     </div>
   )
 }
+
