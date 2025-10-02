212708c886a75d5ff70cbd12ee65a6de9b3ff983
Hagargharib <hagar.mohamed.22230774@commerce.helwan.edu.eg>
2025-10-03 00:25:33 +0300

    Fix TypeScript any errors and improve typings

--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -3,7 +3,10 @@ import CategoriesGrid from "@/components/categories-compo/CategoriesGrid";
 
 
 export default async function CategoriesPage() {
-  const { data: categories } = await getAllCategories();
+  // const { data: categories } = await getAllCategories();
+  const result = await getAllCategories();
+  const categories = result?.data ?? [];
+
 
   return (
     <>
